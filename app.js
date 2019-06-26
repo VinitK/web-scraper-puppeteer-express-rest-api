5aaeafa251ddfaf0b8f2b97f56006339f7a2eeed
vinitcutshort <vinit@cutshort.io>
2019-06-26 10:27:31 +0530

    Home UI added

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ const scraper = require('./utils/scraper')
 
 const app = express();
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 app.get('/reviews', (req, res) => {
     scraper.extractReviews(req.query.url)
     .then(data => {
         res.status(200).json({ message: "success", data: data })
-    }).catch(err => res.status(500).json({ message: "Something went wrong. Could not fetch result." }))
+    }).catch(err => res.status(500).json({ message: err }))
 });
 
-app.listen(process.env.PORT || 3000, () =>
-  console.log('Example app listening on port!'),
+app.get('/', (req, res) => {
+  res.status(200).render('home');
+});
+
+app.listen(process.env.PORT || 5000, () =>
+  console.log('Listening on port!'),
 );
