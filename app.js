c5bbba473440785ed9bc98124b6c1acdda59845d
vinitcutshort <vinit@cutshort.io>
2019-06-26 15:52:29 +0530

    Stable with UI

--- app.js
+++ app.js
@@ -10,8 +10,25 @@ app.set('view engine', 'ejs');
 app.get('/reviews', (req, res) => {
     scraper.extractReviews(req.query.url)
     .then(data => {
-        res.status(200).json({ message: "success", data: data })
-    }).catch(err => res.status(500).json({ message: err }))
+      if(data.reviewCount > 0) {
+        data.message = "success";
+      } else if (data.reviewCount == 0) {
+        data.message = "No data";
+      } else {
+        data.message = "failed";
+      }
+      res.status(200).render('reviews', data);
+    }).catch(err => {
+      res.status(500).render('reviews', { message:'reviews', url: req.query.url });
+    });
+});
+
+app.get('/api/reviews', (req, res) => {
+  scraper.extractReviews(req.query.url)
+  .then(data => {
+    data.message = data ? "success" : "fail";
+    res.status(200).json(data);
+  }).catch(err => res.status(500).json({ message: err }))
 });
 
 app.get('/', (req, res) => {
